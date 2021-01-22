6f89f340b4f2a9fa381e0dd9aa0237a12f470b0f
Aurélien Strimelle <aurelien.strimelle@bstorm.be>
2021-01-22 10:36:09 +0100

    Add console app for test of DAL

---
+++ Demo_Redline_ASPMVC.ConsommationDAL/Program.cs
@@ -0,0 +1,28 @@
+﻿using Demo_Redline_ASPMVC.DAL.Entities;
+using Demo_Redline_ASPMVC.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Redline_ASPMVC.ConsommationDAL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("GetAll GenreRepository");
+            GenreRepository genreRepo = new GenreRepository();
+
+            IEnumerable<Genre> genres = genreRepo.GetAll();
+            foreach (Genre g in genres)
+            {
+                Console.WriteLine($"\t {g.Id} - {g.Name}");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
