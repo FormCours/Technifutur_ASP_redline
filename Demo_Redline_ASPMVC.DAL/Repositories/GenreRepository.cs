922fff98eb44a096962a8e1a8f689633f0f9fe52
Aurélien Strimelle <aurelien.strimelle@bstorm.be>
2021-01-22 10:47:08 +0100

    Update getAll methode with using

--- Demo_Redline_ASPMVC.DAL/Repositories/GenreRepository.cs
+++ Demo_Redline_ASPMVC.DAL/Repositories/GenreRepository.cs
@@ -22,46 +22,80 @@ namespace Demo_Redline_ASPMVC.DAL.Repositories
         // Méthode du CRUD
 
         #region Premiere version du GetGall
+        //public IEnumerable<Genre> GetAll()
+        //{
+        //    // Pour se connecter à SLQ server
+        //    SqlConnection connection = new SqlConnection(_ConnectionString);
+
+        //    // Creation de la commande SQL
+        //    SqlCommand command = connection.CreateCommand();
+
+        //    command.CommandType = System.Data.CommandType.Text;
+        //    command.CommandText = "SELECT Id_Genre, Name FROM genre;";
+
+        //    // Ouverture de la connexion
+        //    connection.Open();
+
+        //    // Execution de la requete pour obtenir un "reader" => Col/Row
+        //    SqlDataReader reader = command.ExecuteReader();
+
+        //    // Parcours des données de la requete
+        //    List<Genre> genres = new List<Genre>();
+        //    while(reader.Read())
+        //    {
+        //        // Recuperation des données de la Row
+        //        long idGenre = (long)reader["Id_Genre"];
+        //        string name = reader["Name"].ToString();
+
+        //        genres.Add(new Genre(idGenre, name));
+        //    }
+
+        //    // On ferme de reader et la commande
+        //    reader.Close();
+        //    command.Dispose();
+
+        //    // Fermeture de la connexion
+        //    connection.Close();
+        //    connection.Dispose();
+
+        //    return genres;
+        //}
+        #endregion
+        #region Seconde version du GetAll avec "using"
         public IEnumerable<Genre> GetAll()
         {
             // Pour se connecter à SLQ server
-            SqlConnection connection = new SqlConnection(_ConnectionString);
-
-            // Creation de la commande SQL
-            SqlCommand command = connection.CreateCommand();
-
-            command.CommandType = System.Data.CommandType.Text;
-            command.CommandText = "SELECT Id_Genre, Name FROM genre;";
-
-            // Ouverture de la connexion
-            connection.Open();
-
-            // Execution de la requete pour obtenir un "reader" => Col/Row
-            SqlDataReader reader = command.ExecuteReader();
-
-            // Parcours des données de la requete
-            List<Genre> genres = new List<Genre>();
-            while(reader.Read())
+            using (SqlConnection connection = new SqlConnection(_ConnectionString))
             {
-                // Recuperation des données de la Row
-                long idGenre = (long)reader["Id_Genre"];
-                string name = reader["Name"].ToString();
+                // Creation de la commande SQL
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandType = System.Data.CommandType.Text;
+                    command.CommandText = "SELECT Id_Genre, Name FROM genre;";
 
-                genres.Add(new Genre(idGenre, name));
+                    // Ouverture de la connexion
+                    connection.Open();
+
+                    // Execution de la requete pour obtenir un "reader" => Col/Row
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        // Parcours des données de la requete
+                        List<Genre> genres = new List<Genre>();
+                        while (reader.Read())
+                        {
+                            // Recuperation des données de la Row
+                            long idGenre = (long)reader["Id_Genre"];
+                            string name = reader["Name"].ToString();
+
+                            genres.Add(new Genre(idGenre, name));
+                        }
+
+                        return genres;
+                    }
+                }
             }
-
-            // On ferme de reader et la commande
-            reader.Close();
-            command.Dispose();
-
-            // Fermeture de la connexion
-            connection.Close();
-            connection.Dispose();
-
-            return genres;
         }
         #endregion
-
 
 
 
