5bebf86fe09baa7c798d0564ec6bbaef2fb4b037
Aurélien Strimelle <aurelien.strimelle@bstorm.be>
2021-01-22 14:07:11 +0100

    DML operation in GenreRepository

--- Demo_Redline_ASPMVC.ConsommationDAL/Program.cs
+++ Demo_Redline_ASPMVC.ConsommationDAL/Program.cs
@@ -14,7 +14,7 @@ namespace Demo_Redline_ASPMVC.ConsommationDAL
         {
             GenreRepository genreRepo = new GenreRepository();
 
-            Console.WriteLine("GetAll GenreRepository");
+            Console.WriteLine("GenreRepository GetAll");
             IEnumerable<Genre> genres = genreRepo.GetAll();
             foreach (Genre g in genres)
             {
@@ -22,10 +22,16 @@ namespace Demo_Redline_ASPMVC.ConsommationDAL
             }
             Console.WriteLine();
 
-            Console.WriteLine("Get(3) GenreRepository");
+            Console.WriteLine("GenreRepository Get(3)");
             Genre g3 = genreRepo.Get(3);
             Console.WriteLine($"\t {g3.Id} - {g3.Name}");
+            Console.WriteLine();
 
+            Console.WriteLine("GenreRepository Insert");
+            Genre newGenre = new Genre("Demo");
+
+            Genre dbGenre = genreRepo.Insert(newGenre);
+            Console.WriteLine($"\t {dbGenre.Id} - {dbGenre.Name}");
 
             Console.ReadLine();
         }
--- Demo_Redline_ASPMVC.DAL/Repositories/GenreRepository.cs
+++ Demo_Redline_ASPMVC.DAL/Repositories/GenreRepository.cs
@@ -127,23 +127,86 @@ namespace Demo_Redline_ASPMVC.DAL.Repositories
             }
         }
 
-
-
-
-
-        public bool Delete(long key)
-        {
-            throw new NotImplementedException();
-        }
-
         public Genre Insert(Genre entity)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(_ConnectionString))
+            {
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandType = System.Data.CommandType.Text;
+                    command.CommandText = "INSERT INTO [Genre] (Name)"
+                                        + " OUTPUT inserted.Id_Genre, inserted.Name"
+                                        + " VALUES (@name)";
+
+                    command.Parameters.AddWithValue("@name", entity.Name);
+
+
+                    connection.Open();
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        reader.Read();
+                        return new Genre(
+                            (long)reader["Id_Genre"],
+                            reader["Name"].ToString()
+                        );
+                    }
+                }
+            }
         }
 
         public Genre Update(long key, Genre entity)
         {
-            throw new NotImplementedException();
+            using(SqlConnection connection = new SqlConnection(_ConnectionString))
+            {
+                using(SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandType = System.Data.CommandType.Text;
+                    command.CommandText = "UPDATE Genre"
+                                        + " SET [Name] = @name"
+                                        + " OUTPUT inserted.Id_Genre, inserted.Name"
+                                        + " WHERE [Id_Genre] = @id";
+
+                    command.Parameters.AddWithValue("@name", entity.Name);
+                    command.Parameters.AddWithValue("@id", key);
+
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        Genre genre = null;
+                        if(reader.Read())
+                        {
+                            genre = new Genre(
+                                (long)reader["Id_Genre"],
+                                reader["Name"].ToString()
+                            );
+                        }
+                        return genre;
+                    }
+                }
+            }
         }
+
+        public bool Delete(long key)
+        {
+            using(SqlConnection connection = new SqlConnection(_ConnectionString))
+            {
+                using(SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandType = System.Data.CommandType.Text;
+                    command.CommandText = "DELETE FROM Genre WHERE Id_Genre = @id;";
+
+                    SqlParameter paramId = new SqlParameter("@id", key);
+                    command.Parameters.Add(paramId);
+
+                    connection.Open();
+
+                    int nbRow = command.ExecuteNonQuery();
+
+                    return (nbRow == 1);
+                }
+            }
+        }
+
     }
 }
