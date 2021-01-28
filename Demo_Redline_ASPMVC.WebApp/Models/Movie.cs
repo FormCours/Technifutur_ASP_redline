6925c9d74499160379779077916411c7e857f7c5
Aurélien Strimelle <aurelien.strimelle@bstorm.be>
2021-01-28 17:16:24 +0100

    Add model for Movie

---
+++ Demo_Redline_ASPMVC.WebApp/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Demo_Redline_ASPMVC.WebApp.Models
+{
+    public class Movie
+    {
+        public long Id { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime? ReleaseDate { get; set; }
+
+        public string ProductionCompany { get; set; }
+
+        public List<Genre> Genres { get; set; }
+    }
+}
