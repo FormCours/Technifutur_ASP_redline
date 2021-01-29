319ff2ec698ad540332ec48de18171be569b7475
Aurélien Strimelle <aurelien.strimelle@bstorm.be>
2021-01-29 10:47:13 +0100

    Add Mapper and Service for Movie

---
+++ Demo_Redline_ASPMVC.WebApp/Mappers/MovieMapper.cs
@@ -0,0 +1,33 @@
+﻿using Demo_Redline_ASPMVC.DAL.Repositories;
+using Demo_Redline_ASPMVC.WebApp.ServicesData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using C = Demo_Redline_ASPMVC.WebApp.Models;
+using G = Demo_Redline_ASPMVC.DAL.Entities;
+
+namespace Demo_Redline_ASPMVC.WebApp.Mappers
+{
+    public static class MovieMapper
+    {
+        public static C.Movie ToClient(this G.Movie global)
+        {
+            MovieService movieService = new MovieService();
+            String company = movieService.GetCompanyFromMovie(global.IdProductionCompany);
+
+            GenreService genreService = new GenreService();
+            List<C.Genre> genres = genreService.GetFromMovie(global.Id).ToList();
+
+            return new C.Movie()
+            {
+                Id = global.Id,
+                Title = global.Title,
+                ReleaseDate = global.ReleaseDate,
+                ProductionCompany = company,
+                Genres= genres
+            };
+        }
+    }
+}
---
+++ Demo_Redline_ASPMVC.WebApp/ServicesData/MovieService.cs
@@ -0,0 +1,32 @@
+﻿using Demo_Redline_ASPMVC.DAL.Repositories;
+using Demo_Redline_ASPMVC.WebApp.Mappers;
+using Demo_Redline_ASPMVC.WebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Demo_Redline_ASPMVC.WebApp.ServicesData
+{
+    public class MovieService
+    {
+        MovieRepository movieRepository;
+        ProductionCompanyRepository companyRepository;
+
+        public MovieService()
+        {
+            movieRepository = new MovieRepository();
+            companyRepository = new ProductionCompanyRepository();
+        }
+
+        public string GetCompanyFromMovie(long IdProductionCompany)
+        {
+            return companyRepository.Get(IdProductionCompany).Name;
+        }
+
+        public Movie GetLast()
+        {
+            return movieRepository.GetLastInsert().ToClient();
+        }
+    }
+}
